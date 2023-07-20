6f6291ff1ee37aaf7e6fdeade2a978c715b891eb
frankkulak <fs.kulak@gmail.com>
2023-07-19 21:38:50 -0400

    Update dependencies.

--- index.cjs
+++ index.cjs
@@ -3,6 +3,10 @@ const { StringTableLocale } = require("@s4tk/models/enums");
 const { extractFiles } = require("@s4tk/extraction");
 const { eventListener } = require("./logging.cjs");
 
+// NOTE: It's recommended that you extract base files and SDX separately, b/c
+// SDX files may incorrectly overwrite base files. I have not figured out the
+// correct loading order yet.
+
 // TODO: Ensure these path(s) are correct for your system, these ones are for my
 // MacBook. Windows paths should include the drive and use \\ instead of /.
 // Windows also does *not* have an equivalent to "The Sims 4 Packs", you should
@@ -10,7 +14,7 @@ const { eventListener } = require("./logging.cjs");
 const srcDirs = [
   "/Applications/The Sims 4 Packs", // Packs not included in The Sims 4.app
   "/Applications/The Sims 4.app/Contents", // Base Game and most packs
-  "/Users/YOURNAME/Documents/Electronic Arts/The Sims 4/content", // SDX
+  // "/Users/YOURNAME/Documents/Electronic Arts/The Sims 4/content", // SDX
 ];
 
 // TODO: Ensure this will output where you want it. By default, it will be in a
